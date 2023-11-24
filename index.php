952139aee714489f3541138c7b959645074f7eb0
Ivan Garcia <ivangargalan@gmail.com>
2023-11-24 15:56:07 +0100

    Se ha añadido el inicio de sesión,se ha modificado el formulario

--- index.php
+++ index.php
@@ -23,11 +23,11 @@
             <form class="" action="./pages/pagina.php" method="POST">
                 <div class="form-group">
                     <label for="username">Nombre de Usuario</label>
-                    <input type="text" class="form-control" id="username" placeholder="Usuario">
+                    <input type="text" class="form-control" id="username" placeholder="Usuario" name="username">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Contraseña</label>
-                    <input type="password" class="form-control" id="password" placeholder="">
+                    <input type="password" class="form-control" id="password" placeholder="" name="password">
                 </div>
                 <div class="mt-2 text-center">
                         <button type="submit" class="btn btn-primary">Entrar</button>
@@ -37,22 +37,22 @@
              </form>
         </div>
         <?php
-        $cadena_conexion = 'mysql:dbname=joel;host=127.0.0.1';
-        $usuario = 'joel';
-        $clave = 'joel';
+   //     $cadena_conexion = 'mysql:dbname=inventariotalaveracollection;host=127.0.0.1';
+   //     $usuario = 'joel';
+     //    $clave = 'joel';
 
-        try {
+   //      try {
             //Se crea la conexión con la base de datos
-            $bd = new PDO($cadena_conexion, $usuario, $clave);
+    //         $bd = new PDO($cadena_conexion, $usuario, $clave);
             // Opcional en MySQL, dependiendo del controlador 
             // de base de datos puede ser obligatorio
             //$bd->closeCursor();
-            echo "Conexión establecida";
-            //Se cierra la conexión
-            $bd = null;
-        } catch (Exception $e) {
-            echo "Error con la base de datos: " . $e->getMessage();
-        }
+                // echo "Conexión establecida";
+  //           //Se cierra la conexión
+  //           $bd = null;
+  //       } catch (Exception $e) {
+  //           echo "La página no está disponible";
+  //       }
         ?>  
               </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
--- pages/pagina.php
+++ pages/pagina.php
@@ -8,8 +8,41 @@
         <title>Página</title>
     </head>
     <body>
-
+        <?php
+        session_start();
         
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            try {
+                // joel,joel
+                $db = new PDO('mysql:host=localhost;dbname=inventariotalaveracollection', 'joel', 'joel');
+            } catch (PDOException $e) {
+                echo 'La página no está disponible actualmente';
+                exit;
+            }
+
+            // recogemos los datos de usuario
+            $nombre = htmlspecialchars($_POST["username"]);
+            // codificar la contraseña a md5 (o utilizar password_hash)
+            $password = htmlspecialchars($_POST["password"]);
+
+            // sacar el usuario de la base de datos
+            $checkUser = $db->prepare("SELECT codusuario, Nomusuario, Contraseñausuario FROM usuario WHERE Nomusuario = ? AND Contraseñausuario = ?");
+            $checkUser->execute([$nombre, $password]);
+            
+            $count = $checkUser ->rowCount();
+           
+            if($count > 0){
+                echo 'bien';
+            } else {
+                echo $count;
+                echo 'mal';
+            }
+        } else {
+            echo 'No se ha aprendido la página';
+        }
+        ?>
+
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
