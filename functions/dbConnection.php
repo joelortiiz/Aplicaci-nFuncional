e8e64223cd6ae1165dba11f63ca4b6723371d749
Ivan Garcia <ivangargalan@gmail.com>
2023-12-04 14:49:37 +0100

    cambiada la conexion a la base de datos

--- functions/dbConnection.php
+++ functions/dbConnection.php
@@ -2,7 +2,7 @@
 
     try {
         // joel,joel
-        $db = new PDO('mysql:host=localhost;dbname=inventariotalaveracollection', 'joel', 'joel');
+        $db = new PDO('mysql:host=localhost;dbname=inventariotalaveracollection', 'root', '');
     } catch (PDOException $e) {
         echo 'La página no está disponible actualmente';
         exit;
